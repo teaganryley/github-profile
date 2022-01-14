bcf78a95d10b01415b0f4bbf2643aafc85800fa0
tea <teaganschultz@gmail.com>
2022-01-13 19:47:21 -0500

    fontsource

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@mui/material/CssBaseline';
 import {
   BrowserRouter,
   Routes,
   Route,
 } from 'react-router-dom';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import App from './routes/app/App';
 import Profile from './routes/profile';
 import Home from './routes/home';
@@ -12,6 +17,7 @@ import NotFound from './routes/notFound';
 
 ReactDOM.render(
   <BrowserRouter>
+    <CssBaseline />
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
